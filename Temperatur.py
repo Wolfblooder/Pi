922f41cf63e6f5ed18fb1c3af7f237e6df6759e4
Wolfblooder <72792154+Wolfblooder@users.noreply.github.com>
2021-10-05 10:11:05 +0200

    Ampel integration

--- Temperatur.py
+++ Temperatur.py
@@ -2,9 +2,17 @@ import time
 import RPi.GPIO as GPIO
 
 sensor = '/sys/bus/w1/devices/28-01201c974288/w1_slave'
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(21, GPIO.OUT)
-GPIO.setwarnings(False)
+GPIO.setup(20, GPIO.OUT)
+GPIO.setup(16, GPIO.OUT)
+
+#maxtemp muss >= 18 sein
+maxTemp = 24
+warningArea = 1
+scale = 1
+
 def readTempSensor(sensorName) :
     
     f = open(sensorName, 'r')
@@ -22,26 +30,45 @@ def turnToTemp(temperatur):
 def currentTemp():
     return turnToTemp(extract(readTempSensor(sensor)))
 
-def lampOn():
+def red():
     GPIO.output(21, GPIO.HIGH)
+   
+def yellow():
+    GPIO.output(20, GPIO.HIGH)
 
-def lampOff():
+def green():
+    GPIO.output(16, GPIO.HIGH)
+   
+
+def off():
+    GPIO.output(20, GPIO.LOW)
     GPIO.output(21, GPIO.LOW)
+    GPIO.output(16, GPIO.LOW)
+
 
 def graph(temperatur):
-    i = int((temperatur-18)*8)
-    return ("█" * i)
+    i = int((temperatur-(18))*8*scale)
+    i = ("█" * i)
+    if(len(i)< (48*scale - (24 - maxTemp)*8*scale)):
+        i = i + (" " * (47*scale -((24 - maxTemp)*8*scale)- len(i))) + "|"
+    return i
 
 while True :
     temperatur = currentTemp()
-    led = "☒"
+    led = ""
     tempSpacing = (" " * (8 - (len(str(temperatur)))))
     
-    if(temperatur >= 24):
-        lampOn()
-        led = "☑"
+    if(temperatur >= maxTemp):
+        off()
+        red()
+        led = "☑ "
+    elif(temperatur >= (maxTemp - warningArea)):
+        off()
+        yellow()
+        led = "☒ "
     else:
-        lampOff()
-        led = "☒"
-    print(time.strftime('%H:%M:%S') +" - " + str(temperatur)  + str(tempSpacing) + led + graph(temperatur))
+        off()
+        green()
+        led = "☒ "
+    print(time.strftime('%H:%M:%S') +" | " + str(temperatur)  + str(tempSpacing) + led + graph(temperatur))
     
